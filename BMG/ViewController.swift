1d1e2679bfd6e6281585c757a95e1f993ebfab06
Emma Walker <walkere@tcd.ie>
2019-10-11 13:49:10 +0100

    Initial Commit

---
+++ BMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMG
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 11/10/2019.
+//  Copyright © 2019 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
