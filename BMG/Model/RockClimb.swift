07f021e478720e7005a84fa16da4443550aa57c9
Andrew Wilson-Jones <andrew.wilson-jones@bbc.co.uk>
2019-10-23 11:19:15 +0100

    Model for Rock Climb created

--- BMG/Model/RockClimb.swift
+++ BMG/Model/RockClimb.swift
@@ -10,3 +10,24 @@ import Foundation
 
 
 // Struct containing a single rock climb
+struct RockClimb {
+    
+    let routeName:String
+    let adjGradeRoute:String
+    let techGradeRoute: String
+    let venueName:String
+    let date: Date
+    let partnersName:String
+    let climbingStyle:String
+    
+    init(routeName:String, adjGradeRoute:String, techGradeRoute: String, venueName:String, date: Date, partnersName:String, climbingStyle:String) {
+        self.routeName = routeName
+        self.adjGradeRoute = adjGradeRoute
+        self.techGradeRoute = techGradeRoute
+        self.venueName  = venueName
+        self.date   = date
+        self.partnersName = partnersName
+        self.climbingStyle = climbingStyle
+    }
+}
+
