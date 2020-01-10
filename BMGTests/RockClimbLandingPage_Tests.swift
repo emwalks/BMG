ab149672481aaf3af5ac84b68999fda41a115a5c
Emma Walker <walkere@tcd.ie>
2020-01-10 11:57:01 +0000

    failing unit test for rock climb landing screen

---
+++ BMGTests/RockClimbLandingPage_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  RockClimbLandingPage_Tests.swift
+//  BMGTests
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 10/01/2020.
+//  Copyright © 2020 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import XCTest
+
+class RockClimbLandingPage_Tests: XCTestCase {
+
+    func testThatTheRockClimbLandingPageExists (){
+        
+        let rockClimbLandingPage: Any? = nil
+        
+        XCTAssertNotNil(rockClimbLandingPage, "The landing page is not nil")
+    }
+   
+}
