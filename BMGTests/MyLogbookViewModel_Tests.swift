6bd2b934906dca7210601bfcb391b5dfeded2c75
Emma Walker <walkere@tcd.ie>
2020-01-14 17:06:40 +0000

    add new test case for MyLogbookVM

---
+++ BMGTests/MyLogbookViewModel_Tests.swift
@@ -0,0 +1,25 @@
+//
+//  MyLogbookViewModel_Tests.swift
+//  BMGTests
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 14/01/2020.
+//  Copyright © 2020 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import XCTest
+
+class MyLogbookViewModel_Tests: XCTestCase {
+    
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbDataIsDisplayed(){
+        
+        //arrange
+        //think about initialising the VC with the function instead of calling a function in view did load
+        //act
+        let actualResult = 
+        let expectedResult = 
+        
+        //assert
+        XCTAssertEqual(actualResult, expectedResult)
+    }
+    
+}
