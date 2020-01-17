837030128a287cacaf94d111135454bdafa06cb2
Emma Walker <walkere@tcd.ie>
2020-01-17 13:43:21 +0000

    add passing tests for all climb parameters to be present on the mock screen

--- BMGTests/LogbookViewModel_Tests.swift
+++ BMGTests/LogbookViewModel_Tests.swift
@@ -11,53 +11,53 @@ import XCTest
 class LogbookViewModel_Tests: XCTestCase {
     
     class MockLogbookViewModel {
-             
-             var databaseService: DatabaseService
-             var mockLogbookScreen: MockLogbookScreen
-             
-             init(_ databaseService: DatabaseService, logbookScreen: MockLogbookScreen) {
-                 self.databaseService = databaseService
-                 self.mockLogbookScreen = logbookScreen
-                 retrieveAllRockClimbData()
-             }
-             
-             func retrieveAllRockClimbData() {
-                 let rockClimbDataArrayReceived = databaseService.getAllRockClimbDataFromDb()
-                 mockLogbookScreen.allRockClimbDataPresented(arrayOfRockClimbs: rockClimbDataArrayReceived)
-             }
-             
-         }
-         
-         class MockLogbookScreen {
-             
-            //optiopnals for test purposes only?
-             public var routeNameOnScreen: String? = nil
-             public var gradeOnScreen: String? = nil
-             public var venueNameOnScreen: String? = nil
-             public var dateOnScreen: String? = nil
-             public var partnersOnScreen: String? = nil
-             public var climbingStyleOnScreen: String? = nil
-             
-             public var arrayOfRockClimbs: Array<RockClimbEntry> = []
-             
-             public var tableOfLoggedClimbs: Bool = false
-             
-             func allRockClimbDataPresented(arrayOfRockClimbs: Array<RockClimbEntry>) {
-                arrayOfRockClimbs.forEach {
-                     (routeNameOnScreen = $0.routeName,
-                     gradeOnScreen = $0.grade,
-                     venueNameOnScreen = $0.venueName,
-                     dateOnScreen = $0.date,
-                     partnersOnScreen = $0.partners,
-                     climbingStyleOnScreen = $0.climbingStyle)
-                 }
-                 tableOfLoggedClimbs = true
-             }
-             
-         }
+        
+        var databaseService: DatabaseService
+        var mockLogbookScreen: MockLogbookScreen
+        
+        init(_ databaseService: DatabaseService, logbookScreen: MockLogbookScreen) {
+            self.databaseService = databaseService
+            self.mockLogbookScreen = logbookScreen
+            retrieveAllRockClimbData()
+        }
+        
+        func retrieveAllRockClimbData() {
+            let rockClimbDataArrayReceived = databaseService.getAllRockClimbDataFromDb()
+            mockLogbookScreen.allRockClimbDataPresented(arrayOfRockClimbs: rockClimbDataArrayReceived)
+        }
+        
+    }
+    
+    class MockLogbookScreen {
+        
+        //optiopnals for test purposes only?
+        public var routeNameOnScreen: String? = nil
+        public var gradeOnScreen: String? = nil
+        public var venueNameOnScreen: String? = nil
+        public var dateOnScreen: String? = nil
+        public var partnersOnScreen: String? = nil
+        public var climbingStyleOnScreen: String? = nil
+        
+        public var arrayOfRockClimbs: Array<RockClimbEntry> = []
+        
+        public var tableOfLoggedClimbs: Bool = false
+        
+        func allRockClimbDataPresented(arrayOfRockClimbs: Array<RockClimbEntry>) {
+            arrayOfRockClimbs.forEach {
+                (routeNameOnScreen = $0.routeName,
+                 gradeOnScreen = $0.grade,
+                 venueNameOnScreen = $0.venueName,
+                 dateOnScreen = $0.date,
+                 partnersOnScreen = $0.partners,
+                 climbingStyleOnScreen = $0.climbingStyle)
+            }
+            tableOfLoggedClimbs = true
+        }
+        
+    }
     
     func testThatWhenLogbookScreenIsPresentedATableOfLoggedClimbsIsDisplayed(){
-    
+        
         //arrange
         
         let mockDatabaseService = MockDatabaseService()
@@ -72,7 +72,7 @@ class LogbookViewModel_Tests: XCTestCase {
         XCTAssertEqual(actualResult, true, "the table of Rock Climbs exists in MockMyLogbookScreen")
     }
     
-     func testThatWhenLogbookScreenIsPresentedTheLoggedClimbRouteNamesAreDisplayed(){
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbRouteNamesAreDisplayed(){
         
         //arrange
         
@@ -81,9 +81,72 @@ class LogbookViewModel_Tests: XCTestCase {
         
         //act
         _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
-        print(mockLogbookScreen.routeNameOnScreen!)
         //assert
         XCTAssertNotNil(mockLogbookScreen.routeNameOnScreen)
     }
     
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbGradesAreDisplayed(){
+        
+        //arrange
+        
+        let mockDatabaseService = MockDatabaseService()
+        let mockLogbookScreen = MockLogbookScreen()
+        
+        //act
+        _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
+        //assert
+        XCTAssertNotNil(mockLogbookScreen.gradeOnScreen)
+    }
+    
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbVenueAreDisplayed(){
+        
+        //arrange
+        
+        let mockDatabaseService = MockDatabaseService()
+        let mockLogbookScreen = MockLogbookScreen()
+        
+        //act
+        _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
+        //assert
+        XCTAssertNotNil(mockLogbookScreen.venueNameOnScreen)
+    }
+    
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbDatesAreDisplayed(){
+         
+         //arrange
+         
+         let mockDatabaseService = MockDatabaseService()
+         let mockLogbookScreen = MockLogbookScreen()
+         
+         //act
+         _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
+         //assert
+         XCTAssertNotNil(mockLogbookScreen.dateOnScreen)
+     }
+    
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbPartnersAreDisplayed(){
+            
+            //arrange
+            
+            let mockDatabaseService = MockDatabaseService()
+            let mockLogbookScreen = MockLogbookScreen()
+            
+            //act
+            _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
+            //assert
+            XCTAssertNotNil(mockLogbookScreen.partnersOnScreen)
+        }
+    
+    func testThatWhenLogbookScreenIsPresentedTheLoggedClimbStyleAreDisplayed(){
+            
+            //arrange
+            
+            let mockDatabaseService = MockDatabaseService()
+            let mockLogbookScreen = MockLogbookScreen()
+            
+            //act
+            _ = MockLogbookViewModel(mockDatabaseService, logbookScreen: mockLogbookScreen)
+            //assert
+            XCTAssertNotNil(mockLogbookScreen.climbingStyleOnScreen)
+        }
 }
