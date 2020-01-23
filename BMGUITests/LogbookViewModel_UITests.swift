33a8484d49f6dfeb70c42dfc7ab0964a72a15f1e
Emma Walker <walkere@tcd.ie>
2020-01-23 16:04:51 +0000

    move reset db code into setup function in UI test using arguments

--- BMGUITests/LogbookViewModel_UITests.swift
+++ BMGUITests/LogbookViewModel_UITests.swift
@@ -23,8 +23,41 @@ class LogbookViewModel_UITests: XCTestCase {
         super.setUp()
         continueAfterFailure = false
         app = XCUIApplication()
-        //may not need this if I have it set in scheme
-        //i.e. could program it or have it run for all tests - to investigate
+        
+        app.launchArguments = ["RESET_DB"]
+               app.launch()
+               app.otherElements.buttons["Log Rock Climb"].tap()
+               
+               let routeField = app.scrollViews.otherElements.textFields["e.g. The Bat"]
+               routeField.tap()
+               routeField.typeText(routeNameEntered)
+               
+               let gradeField = app.scrollViews.otherElements.textFields["E1 5b, E1 5c, etc."]
+               gradeField.tap()
+               app.pickerWheels["M"].adjust(toPickerWheelValue: "VD")
+               app.pickers.children(matching: .pickerWheel).element(boundBy: 1).adjust(toPickerWheelValue: "7b")
+               app.toolbars["Toolbar"].buttons["Done"].tap()
+               
+               let venueField =  app.scrollViews.otherElements.textFields["e.g. Ben Nevis"]
+               venueField.tap()
+               venueField.typeText(venueNameEntered)
+               
+               let dateField = app.scrollViews.otherElements.textFields["DD/MM/YYYY"]
+               dateField.tap()
+               app.pickers.children(matching: .pickerWheel).element(boundBy: 0).adjust(toPickerWheelValue: "January")
+               app.pickers.children(matching: .pickerWheel).element(boundBy: 1).adjust(toPickerWheelValue: "31")
+               app.pickers.children(matching: .pickerWheel).element(boundBy: 2).adjust(toPickerWheelValue: "2020")
+               app.toolbars["Toolbar"].buttons["Done"].tap()
+               
+               let partnersField = app.scrollViews.otherElements.containing(.staticText, identifier:"Route:").children(matching: .textField).element(boundBy: 4)
+               partnersField.tap()
+               partnersField.typeText(partnersEntered)
+               
+               let exitRouteFieldKeyboard = app.scrollViews.otherElements
+               exitRouteFieldKeyboard.staticTexts["Route:"].tap()
+               
+               app.scrollViews.otherElements.tables.staticTexts["Alternate"].tap()
+               app.scrollViews.otherElements.buttons["Submit"].tap()
     }
     
     override func tearDown() {
@@ -32,40 +65,7 @@ class LogbookViewModel_UITests: XCTestCase {
     }
     
     func testThatWhenLogbookScreenIsPresentedATableOfLoggedClimbsIsDisplayed() {
-       app.launchArguments = ["RESET_DB"]
-        app.launch()
-        app.otherElements.buttons["Log Rock Climb"].tap()
-        
-        let routeField = app.scrollViews.otherElements.textFields["e.g. The Bat"]
-        routeField.tap()
-        routeField.typeText(routeNameEntered)
-        
-        let gradeField = app.scrollViews.otherElements.textFields["E1 5b, E1 5c, etc."]
-        gradeField.tap()
-        app.pickerWheels["M"].adjust(toPickerWheelValue: "VD")
-        app.pickers.children(matching: .pickerWheel).element(boundBy: 1).adjust(toPickerWheelValue: "7b")
-        app.toolbars["Toolbar"].buttons["Done"].tap()
-        
-        let venueField =  app.scrollViews.otherElements.textFields["e.g. Ben Nevis"]
-        venueField.tap()
-        venueField.typeText(venueNameEntered)
-        
-        let dateField = app.scrollViews.otherElements.textFields["DD/MM/YYYY"]
-        dateField.tap()
-        app.pickers.children(matching: .pickerWheel).element(boundBy: 0).adjust(toPickerWheelValue: "January")
-        app.pickers.children(matching: .pickerWheel).element(boundBy: 1).adjust(toPickerWheelValue: "31")
-        app.pickers.children(matching: .pickerWheel).element(boundBy: 2).adjust(toPickerWheelValue: "2020")
-        app.toolbars["Toolbar"].buttons["Done"].tap()
-        
-        let partnersField = app.scrollViews.otherElements.containing(.staticText, identifier:"Route:").children(matching: .textField).element(boundBy: 4)
-        partnersField.tap()
-        partnersField.typeText(partnersEntered)
-        
-        let exitRouteFieldKeyboard = app.scrollViews.otherElements
-        exitRouteFieldKeyboard.staticTexts["Route:"].tap()
-        
-        app.scrollViews.otherElements.tables.staticTexts["Alternate"].tap()
-        app.scrollViews.otherElements.buttons["Submit"].tap()
+      
         app.launchArguments = []
         app.launch()
         
